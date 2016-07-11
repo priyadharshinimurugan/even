4b217e7a565a4575446cdda1b53bf0eff8fd8cfa
priyadharshinimurugan <priyakpriet17@gmail.com>
2016-07-11 12:14:26 +0530

    finish

---
+++ odd.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import.utill.scanner;
+class odd
+int a;
+{
+public static void main(sting[]args)
+{
+if(a>0)
+system.out.println('positive');
+else
+system.out.println('negative');
+}
+}
